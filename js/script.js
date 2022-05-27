1d5afb3db5faa74a228323eea7c16192cb76083d
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-05-27 13:36:17 +0300

    The design has been fixed and the Home page is completely finished.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,59 @@
-document.addEventListener('DOMContentLoaded', function (e) {
-    // Döküman hazır olduktan sonra yapılacak işlemler
+// Controle Scroll
+function controleScroll() {
     let nav = document.querySelector('nav');
-    document.addEventListener('scroll', function () {
-        if (document.documentElement.scrollTop > 64) {
-            nav.classList.add('scrolled');
-        } else {
-            nav.classList.remove('scrolled');
-        }
-    });
-})
+    if (document.documentElement.scrollTop > 64) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+}
+
+// Döküman hazır olduktan sonra yapılacak işlemler
+document.addEventListener('DOMContentLoaded', function (e) {
+    controleScroll();
+});
+
+// Scroll olunca navigasyona gölge ekleme
+document.addEventListener('scroll', function () {
+    controleScroll();
+});
+
+
+let sideButton = document.querySelector('.side-button');
+let closeButton = document.querySelector('.close-button');
+let overlay = document.querySelector('.overlay');
+let sideNav = document.querySelector('.side-nav');
+
+// Open Menu
+let menuOpen;
+function openMenu() {
+    menuOpen = true;
+    overlay.classList.add('active');
+    sideNav.classList.add('active');
+    document.body.classList.add('side-bar-open');
+}
+
+
+// Close Menu
+function closeMenu() {
+    menuOpen = false;
+    overlay.classList.remove('active');
+    sideNav.classList.remove('active');
+    document.body.classList.remove('side-bar-open');
+}
+
+sideButton.addEventListener('click', function () {
+    if (!menuOpen) {
+        openMenu();
+    }
+});
+closeButton.addEventListener('click', function () {
+    if (menuOpen) {
+        closeMenu();
+    }
+});
+overlay.addEventListener('click', function () {
+    if (menuOpen) {
+        closeMenu();
+    }
+});
